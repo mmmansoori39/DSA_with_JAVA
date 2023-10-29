21191d3eeb21c0bc2a02f1a67cb41eb32842f99e
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-10-29 11:08:01 +0530

    done

---
+++ 5.Arrays/Arrays.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+
+public class Arrays {
+
+    public static void main(String args[]) {
+        // int[] arr = new int[3];
+        int arr[] = new int[3];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arr[i]);
+
+        }
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+    }
+}
