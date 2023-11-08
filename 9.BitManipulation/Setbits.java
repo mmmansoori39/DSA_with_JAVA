a6f26b543011be99c30fb319edbec8b2262d640a
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-08 11:32:47 +0530

    file added

--- 9.BitManipulation/Setbits.java
+++ 9.BitManipulation/Setbits.java
@@ -1,3 +1,15 @@
 public class Setbits {
-    
+    public static void main(String[] args) {
+        // set the 2rd bit ( position = 1) of a number n. (n = 0101)
+    // Bit mask : 1<<i
+    // operation : OR
+
+    int n = 5;
+    int pos = 1;
+    int bitMask = 1<<pos;
+
+    int output = bitMask | n;
+    System.out.println(output);
+    }
+
 }
