818dc25c5cce4be49758e819424340cc0292101a
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-08 11:17:07 +0530

    file added

---
+++ 9.BitManipulation/GetBit.java
@@ -0,0 +1,9 @@
+public class GetBit {
+    public static void main(String[] args) {
+        // get the 3rd bit ( position = 2) of a number n. (n = 0101)
+        // Bit mask : 1<<i
+        // operation : AND
+
+        
+    }
+}
