f5190516c1529d669bfe87ba7ba685644ddf3bd1
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-10 22:32:12 +0530

    file added

--- 10.Sorting/InsertionSort.java
+++ 10.Sorting/InsertionSort.java
@@ -1,6 +1,25 @@
 public class InsertionSort {
     public static void main(String[] args) {
-        Sy
+        int arr[] = { 7, 8, 3, 1, 2, 5 };
+
+        // insertion sort
+        // time complexity = 0(n^2)
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j= i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+
+            //placement
+            arr[j+1] = current;
+        }
+
+        // for printing sorted array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
         
     }
 }
