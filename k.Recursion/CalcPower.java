be5d203ce98cc5efd2bd7a6fe122b7d82a80d93c
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-14 15:53:59 +0530

    some code added

--- k.Recursion/CalcPower.java
+++ k.Recursion/CalcPower.java
@@ -1,4 +1,6 @@
 public class CalcPower {
+
+    // stack height = n
     public static int calcPower(int x, int n){
 
         // base case 1
@@ -15,8 +17,31 @@ public class CalcPower {
         int xPown = x * xPownm1;
         return xPown;
     }
+
+    // stack height = logn
+    public static int calcPower1(int x, int n){
+
+        // base case 1
+        if ( n == 0){
+            return 1;
+        }
+
+        // base case 2
+        if ( x== 0){
+            return 0;
+        }
+        // if n is even then
+        if ( n%2 == 0){
+            return calcPower1(x, n/2)*calcPower1(x, n/2);
+        }
+        else{
+            return calcPower1(x, n/2)*calcPower1(x, n/2)*x;
+        }
+    }
     public static void main(String[] args) {
         int result = calcPower(2, 5);
+        int result1 = calcPower1(2, 5);
         System.out.println(result);
+        System.out.println(result1);
     }
 }
