d81435f754524bc25d3f904b9fd06c5d2c051c2d
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-15 10:37:56 +0530

    problem solved

--- k.Recursion/FindStrictedSortedArray.java
+++ k.Recursion/FindStrictedSortedArray.java
@@ -1,3 +1,18 @@
 public class FindStrictedSortedArray {
-    
+    public static boolean isSorted(int arr[], int idx){
+        // base case
+        if ( idx == arr.length-1){
+            return true;
+        }
+        if ( arr[idx] >= arr[idx+1]){
+            return false;
+        }
+        else{
+            return isSorted(arr, idx+1);
+        }
+    }
+    public static void main( String args[]){
+        int arr[] = {1,2,3,4,3,5};
+        System.out.println(isSorted(arr, 0));
+    }
 }
