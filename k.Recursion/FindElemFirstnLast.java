9adddec0aded4edff4960146bcbd7a4e24262257
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-15 10:24:17 +0530

    problem solved

---
+++ k.Recursion/FindElemFirstnLast.java
@@ -0,0 +1,28 @@
+// find the first and last occurance of an element in string
+
+public class FindElemFirstnLast {
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void elementOccurance( String str, int idx, char element){
+        // base case
+        if ( idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+        }
+        char currElem = str.charAt(idx);
+        if ( currElem == element){
+            if ( first == -1){
+                first = idx;
+            }
+            else{
+                last = idx;
+            }
+        }
+        elementOccurance(str, idx+1, element);
+    }
+    public static void main(String args[]) {
+        String str = "abcsaba";
+        elementOccurance(str, 0, 'a');
+    }
+}
