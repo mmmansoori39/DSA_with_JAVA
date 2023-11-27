c316b44ebf5cd821d1d63ed9d517382c631f13b8
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-27 10:25:55 +0530

    added

---
+++ i.BitManipulation/BitwiseOperation.java
@@ -0,0 +1,9 @@
+public class BitwiseOperation {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        int result = a | b;
+        int result1 = ~a;
+        System.out.println(result);
+        System.out.println(result1 );
+    }
+}
