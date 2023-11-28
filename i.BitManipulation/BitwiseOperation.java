5ebeacc4b74478484d311747a27aa3e2eb6c262a
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-28 12:27:49 +0530

    code added

--- i.BitManipulation/BitwiseOperation.java
+++ i.BitManipulation/BitwiseOperation.java
@@ -1,9 +1,17 @@
 public class BitwiseOperation {
     public static void main(String[] args) {
-        int a = 3, b = 5;
+        int a = 3, b = 71;
         int result = a | b;
         int result1 = ~a;
-        System.out.println(result);
-        System.out.println(result1 );
+        // System.out.println(result);
+        // System.out.println(result1 );
+
+        int i = 0;
+        while( b!=0){
+            int bit = b&1;
+            b>>>=1;
+            System.out.println(bit);
+            i++;
+        }
     }
 }
---
+++ i.BitManipulation/Powe.java
@@ -0,0 +1,5 @@
+public class Powe {
+    public static void main(String[] args) {
+     
+    }
+}
