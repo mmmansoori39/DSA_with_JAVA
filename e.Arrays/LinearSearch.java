e3fe63606065531429746716283539e40447c82f
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-29 09:49:49 +0530

    code added

--- e.Arrays/LinearSearch.java
+++ e.Arrays/LinearSearch.java
@@ -1,1 +1,23 @@
+public class LinearSearch {
 
+    public static boolean linearSearch( int arr[], int size, int key){
+        for( int i=0; i<size; i++){
+            if(arr[i] == key){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {-12, 23, 43, -4, 9, 0, 24};
+        int key = 63;
+        boolean result = linearSearch(arr, arr.length, key);
+        if(result){
+            System.out.println("Element is found");
+        }
+        else{
+            System.out.println("Element is not found");
+        }
+    }
+}
