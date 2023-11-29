fb5013a38f1059e00d03f5b632d19eafe7d98863
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-11-29 10:44:09 +0530

    code added

--- Problems/Array/FindDuplicates.java
+++ Problems/Array/FindDuplicates.java
@@ -1,8 +1,21 @@
-import java.util.*;
+import java.util.HashSet;
 
 public class FindDuplicates {
+    public static int findDuplicate(int arr[], int n) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : arr) {
+            if (!set.add(num)) {
+                return num;
+            }
+        }
+        
+        return -1; // Return -1 if no duplicate is found
+    }
 
-    public static void main(String args[]){
-
+    public static void main(String args[]) {
+        int arr[] = { 1, 4, 2, 6, 8, 2 };
+        System.out.println(findDuplicate(arr, arr.length));
     }
 }
+
