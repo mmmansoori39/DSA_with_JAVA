d6ac9b5c5afe67c775103e86da508d81e51857e0
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2023-12-20 10:00:52 +0530

    updated

--- c.Pattern/BAbberStar.java
+++ c.Pattern/BAbberStar.java
@@ -20,11 +20,31 @@ public class BAbberStar {
         }
     }
 
-    
+    public static void triPattern(int n){
+        for ( int i=1; i<=n; i++){
+            for ( int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void triNum(int n){
+        for ( int i=1; i<=n; i++){
+            for ( int j=1; j<=i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        int n = 4;
-        squareNum(n);
-        squareContiNum(n);
+        int n = 8;
+        // squareNum(n);
+        // squareContiNum(n);
+        triPattern(n);
+        triNum(n);
     }
 }
